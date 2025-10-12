cfdc5ab6892c98a0da945f57ff8f7c25652b42e4
HeadShotXx <148909166+HeadShotXx@users.noreply.github.com>
2025-10-12 13:42:19 +0300

    Delete src directory

--- src/main.rs
+++
@@ -1,9 +0,0 @@
-use anti_vm::is_virtualized;
-
-fn main() {
-    if is_virtualized() {
-        println!("A virtual machine was detected.");
-    } else {
-        println!("No virtual machine was detected.");
-    }
-}
