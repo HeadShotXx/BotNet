a783f2af3894a20d0499dcbe0fde5221515f03a7
PayloadExecuter <148909166+HeadShotXx@users.noreply.github.com>
2026-04-14 20:13:51 +0300

    Delete payload.c

--- payload.c
+++
@@ -1,8 +0,0 @@
-#include <windows.h>
-
-int main() {
-    MessageBoxA(NULL, "Payload executed successfully!", "Shellcode Test", MB_OK);
-    return 0;
-}
-
-// Compilation: x86_64-w64-mingw32-gcc payload.c -o payload.exe -mwindows
